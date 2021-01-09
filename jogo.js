00b628f92d0b3bd7747149e77c1599739f0dcd8d
Pri Pinheiro <pripinheiro.sa@gmail.com>
2021-01-09 16:47:07 -0300

    add niveis

--- jogo.js
+++ jogo.js
@@ -2,6 +2,20 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 30
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if(nivel === 'normal') {
+	criaMosquitoTempo = 1500
+} else if(nivel === 'dificil') {
+	criaMosquitoTempo = 1000
+} else if(nivel === 'chucknorris') {
+	criaMosquitoTempo = 750
+}
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -11,6 +25,20 @@ function ajustaTamanhoPalcoJogo() {
 }
 
 ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+
+	tempo -= 1
+
+	if(tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosquito)
+		window.location.href = 'vitoria.html'
+	} else {
+		document.getElementById('cronometro').innerHTML = tempo
+	}
+	
+}, 1000)
 
 
 function posicaoRandomica() {
